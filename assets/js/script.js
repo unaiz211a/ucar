61e606f87bbd9be5a982df78349b9ae7234b00bc
unaiz211a <131944649+unaiz211a@users.noreply.github.com>
2023-08-19 10:31:47 +0500

    car
    car

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+
+$('.nav-btn').on('click',function(){
+    $('.nav-u ').addClass('trans0')
+  })
+
+  $('.cls-btn').on('click',function(){
+    $('.nav-u').removeClass('trans0')
+  })
+
+  $('.ques-1').on('click',function(){
+    $('.ans-1').toggleClass('active-u');
+    $('.i-u1').toggleClass('rotate');
+})
+$('.ques-2').on('click',function(){
+    $('.ans-2').toggleClass('active-u');
+    $('.i-u2').toggleClass('rotate');
+})
+$('.ques-3').on('click',function(){
+    $('.ans-3').toggleClass('active-u');
+    $('.i-u3').toggleClass('rotate');
+})
+$('.ques-4').on('click',function(){
+    $('.ans-4').toggleClass('active-u');
+    $('.i-u4').toggleClass('rotate');
+})
+$('.ques-5').on('click',function(){
+    $('.ans-5').toggleClass('active-u');
+    $('.i-u5').toggleClass('rotate');
+})
+$('.ques-6').on('click',function(){
+    $('.ans-6').toggleClass('active-u');
+    $('.i-u6').toggleClass('rotate');
+})
+$('.ques-7').on('click',function(){
+    $('.ans-7').toggleClass('active-u');
+    $('.i-u7').toggleClass('rotate');
+})
+$('.ques-8').on('click',function(){
+    $('.ans-8').toggleClass('active-u');
+    $('.i-u8').toggleClass('rotate');
+})
